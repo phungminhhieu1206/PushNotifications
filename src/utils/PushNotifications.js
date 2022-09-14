5d7ded82c1bd4fc5b6743a645cfee70fd6cf1dba
Phung Minh Hieu <hieuphung1206@Phungs-MacBook-Pro.local>
2022-09-14 17:15:53 +0700

    done setup push notification with firebase reactnative

---
+++ src/utils/PushNotifications.js
@@ -0,0 +1,57 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import messaging from '@react-native-firebase/messaging';
+
+export async function requestUserPermission() {
+  const authStatus = await messaging().requestPermission();
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+  if (enabled) {
+    console.log('Authorization status:', authStatus);
+    getFcmToken();
+  }
+}
+
+const getFcmToken = async () => {
+  let fcmToken = await AsyncStorage.getItem('fcmToken');
+  console.log('Old Fcm Token:', fcmToken);
+
+  if (!fcmToken) {
+    try {
+      const fcmToken = await messaging().getToken();
+      if (fcmToken) {
+        console.log('new Generated Fcm Token: ', fcmToken);
+        await AsyncStorage.setItem('fcmToken', fcmToken);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
+
+export const NotificationServices = () => {
+  messaging().onNotificationOpenedApp(remoteMessage => {
+    console.log(
+      'Notification caused app to open from background state:',
+      remoteMessage.notification,
+    );
+  });
+
+  // Foreground Message Handling
+  messaging().onMessage(async remoteMessage => {
+    console.log('Notification in Foreground:', remoteMessage);
+  });
+
+  // Check whether an initial notification is available
+  messaging()
+    .getInitialNotification()
+    .then(remoteMessage => {
+      if (remoteMessage) {
+        console.log(
+          'Notification caused app to open from quit state:',
+          remoteMessage.notification,
+        );
+      }
+    });
+};
